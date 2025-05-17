5632b5d291b844780d5434551f4590051cc82d23
roriobarnoh <roriokiptoo2016@gmail.com>
2025-05-17 12:05:31 +0300

    fileio

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    file_path = f'{file_name}.txt'
+    with open(file_path, "w") as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_path = f'{file_name}.txt'
+    with open(file_path, "a") as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_path = f'{file_name}.txt'
+    with open(file_path, 'r') as f:
+        return f.read()
+
